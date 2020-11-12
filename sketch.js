fc83b3c18911f66d018b16ac65ea5b4041449edd
Agnibho1678 <73741624+Agnibho1678@users.noreply.github.com>
2020-11-12 18:34:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.8});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5});
 	World.add(world, packageBody);
 	
 
@@ -58,7 +58,7 @@ function draw() {
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.8});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5});
 	World.add(world, packageBody);
 }
 
